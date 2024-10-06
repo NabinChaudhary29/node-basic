df63ddb5b9f9ac3009acfe8982cad50c6fa4f1d1
Nabin Chaudhary <nabinchy29@gmail.com>
2024-10-06 23:54:33 +1100

    not to do list started

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 // const express = require('express')
 import express from "express";
 import path from "path";
-
+import fs from "fs";
 const app = express();
 const __dirname = path.resolve();
 const PORT = 3000;
 import {home,login,register} from './src/home.js';
+const Task_Data = "./data/tasks.json";
 
 app.use("/static",express.static(path.join(__dirname,"public")));
 
-
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // app.get('/user',  (request, response) => {
 //   console.log("user path");
@@ -40,6 +42,7 @@ app.use("/static",express.static(path.join(__dirname,"public")));
     app.get('/login',  (request, response) => {
         let htmlString = login();
         response.send(htmlString);
+        console.log("LOGIN QUERY:", request.query);
         // response.sendFile (__dirname +"/src/file.json")
     })
 
@@ -48,6 +51,86 @@ app.use("/static",express.static(path.join(__dirname,"public")));
         response.send(htmlString);
         // response.sendFile (__dirname +"/src/file.json")
     })
+
+    app.post("/register", (request, response) => {
+        let { email, name, password } = request.body;
+        console.log(email, name, password);
+        // saving data to database
+      });
+
+    //   app.get("/user",(req,res)=>{
+    //     res.send(500,"userlist");
+    //   })
+
+    //   app.get("/user",(req,res)=>{
+    //     res.status(500).send("data");
+    //   })
+
+      app.get("/user",(req,res)=>{
+        const { email,  password } = req.query;
+        try{
+            
+            res.status(200).send("get user data"); 
+        }
+        catch (error) {
+            res.status(500).send("Error in Server");
+        }
+     })
+     app.post("/user",(req,res)=>{
+        const { email,  password } = req.body;
+
+        try{
+            
+            res.status(200).send(`${email}===== ${password}`); 
+        }
+        catch (error) {
+            res.status(500).send("Error in Server");
+        }
+     })
+     app.put ("/user",(req,res)=>{
+        try{
+            
+            res.status(200).send(" put user data"); 
+        }
+        catch (error) {
+            res.status(500).send("Error in Server");
+        }
+     })
+     app.patch("/user",(req,res)=>{
+        try{
+            
+            res.status(200).send("patch user data"); 
+        }
+        catch (error) {
+            res.status(500).send("Error in Server");
+        }
+     })
+     app.delete("/user",(req,res)=>{
+        try{
+            
+            res.status(200).send(" delete user data"); 
+        }
+        catch (error) {
+            res.status(500).send("Error in Server");
+        }
+     })
+
+
+     //ntdl apis
+
+
+     app.get("/ntdl/tasks",(req,res)=>{
+        //fetch data from database
+        fs.readFile(Task_Data,(error,data)=>{
+            if(error){
+                const errorObj = {
+                    sta
+                }
+            }
+        })
+     })
+
+
 app.listen(PORT, (error)=>{
     error 
     ? console.log("Error in serving") 
